b694858a415b995bdd48bd23ccb9b8c3178695e5
Alexey Zagrebin <azagrebin244@gmail.com>
2024-03-05 22:35:06 +0300

    added zipkin

--- auth-service/docker-container-run.sh
+++ auth-service/docker-container-run.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-docker run -it --name auth-service -p 9000:9000 -e EUREKA_HOST="172.17.0.1" -d auth-service
+docker run -it --name auth-service -p 9000:9000 -e EUREKA_HOST="172.17.0.1" -e ZIPKIN_HOST="172.17.0.1" -e LOGSTASH_HOST="172.17.0.1" -d auth-service
--- exchange-processing/src/main/java/ru/zagrebin/processing/exchangeprocessing/service/ResolveUserService.java
+++ exchange-processing/src/main/java/ru/zagrebin/processing/exchangeprocessing/service/ResolveUserService.java
@@ -2,9 +2,11 @@ package ru.zagrebin.processing.exchangeprocessing.service;
 
 import org.springframework.boot.autoconfigure.security.oauth2.resource.ResourceServerProperties;
 import org.springframework.boot.autoconfigure.security.oauth2.resource.UserInfoRestTemplateFactory;
+import org.springframework.cloud.sleuth.instrument.web.mvc.TracingClientHttpRequestInterceptor;
 import org.springframework.security.oauth2.client.OAuth2RestTemplate;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
 import java.util.Map;
 import java.util.Optional;
 
@@ -12,15 +14,19 @@ import java.util.Optional;
 public class ResolveUserService {
     private final UserInfoRestTemplateFactory restTemplateFactory;
     private final ResourceServerProperties resource;
+    private final TracingClientHttpRequestInterceptor interceptor;
 
 
-    public ResolveUserService(UserInfoRestTemplateFactory restTemplateFactory, ResourceServerProperties resource) {
+    public ResolveUserService(UserInfoRestTemplateFactory restTemplateFactory, ResourceServerProperties resource, TracingClientHttpRequestInterceptor interceptor) {
         this.restTemplateFactory = restTemplateFactory;
         this.resource = resource;
+        this.interceptor = interceptor;
     }
 
     public Long resolveUserId() {
         OAuth2RestTemplate userInfoRestTemplate = restTemplateFactory.getUserInfoRestTemplate();
+
+        userInfoRestTemplate.setInterceptors(List.of(interceptor));
 
         Map<String, Object> response = userInfoRestTemplate.getForEntity(resource.getUserInfoUri(), Map.class).getBody();
         return Optional.ofNullable(response)
